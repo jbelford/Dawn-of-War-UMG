b57bf5900b7f9bc6f4a07b0d11881a466195b36d
Jack Belford <jackbelford12@live.ca>
2024-03-05 00:38:30 -0800

    Disable button when no maps

--- src/DowUmg.Presentation/ViewModels/Generation/GenerationSettingsViewModel.cs
+++ src/DowUmg.Presentation/ViewModels/Generation/GenerationSettingsViewModel.cs
@@ -43,61 +43,22 @@ namespace DowUmg.Presentation.ViewModels
                 }
             );
 
-            GenerateMatchupAction = ReactiveCommand.CreateFromObservable(() =>
-            {
-                var settings = new GenerationSettings()
-                {
-                    Mod = ModViewModel.SelectedItem.GetItem<DowMod>(),
-                    Maps = GeneralTabViewModel
-                        .Maps.Items.Where(map => map.ToggleItem.IsToggled)
-                        .Select(map => map.Model)
-                        .ToList(),
-                    Rules = GeneralTabViewModel
-                        .WinConditions.Items.Where(rule => rule.ToggleItem.IsToggled)
-                        .Select(rule => rule.Model)
-                        .ToList(),
-                    Races = TeamTabViewModel
-                        .Races.Items.Where(race => race.ToggleItem.IsToggled)
-                        .Select(race => race.Model)
-                        .ToList(),
-                    Players = TeamTabViewModel
-                        .Players.Select(player => player.Name.Trim())
-                        .ToList(),
-                    MinComputer = TeamTabViewModel.MinComputers.SelectedItem.GetItem<int>(),
-                    MaxComputer = TeamTabViewModel.MaxComputers.SelectedItem.GetItem<int>(),
-                    MinTeams = TeamTabViewModel.MinTeams.SelectedItem.GetItem<int>(),
-                    MaxTeams = TeamTabViewModel.MaxTeams.SelectedItem.GetItem<int>(),
-                    RandomPositions = TeamTabViewModel.RandomPositions,
-                    OneRaceTeams = TeamTabViewModel.OneRaceTeams,
-                };
-
-                foreach (var diff in GameTabViewModel.DiffOption)
-                {
-                    settings.GameDifficultyTickets[(int)diff.Model] = diff.NumberInput.Input;
-                }
-
-                foreach (var speed in GameTabViewModel.SpeedOption)
-                {
-                    settings.GameSpeedTickets[(int)speed.Model] = speed.NumberInput.Input;
-                }
-
-                foreach (var rate in GameTabViewModel.RateOption)
-                {
-                    settings.ResourceRateTickets[(int)rate.Model] = rate.NumberInput.Input;
-                }
-
-                foreach (var start in GameTabViewModel.StartingOption)
-                {
-                    settings.StartResourceTickets[(int)start.Model] = start.NumberInput.Input;
-                }
-
-                return HostScreen.Router.Navigate.Execute(
-                    new MatchupViewModel(HostScreen, settings)
-                );
-            });
-
             this.WhenActivated(d =>
             {
+                var canGenerateMatchup = this.WhenAnyObservable(x =>
+                        x.GeneralTabViewModel.Maps.CountChanged
+                    )
+                    .Select(count => count > 0)
+                    .ObserveOn(RxApp.MainThreadScheduler);
+
+                GenerateMatchupAction = ReactiveCommand.CreateFromObservable(
+                    () =>
+                        HostScreen.Router.Navigate.Execute(
+                            new MatchupViewModel(HostScreen, CreateGenerationSettings())
+                        ),
+                    canGenerateMatchup
+                );
+
                 this.WhenAnyValue(x => x.ModViewModel.SelectedItem)
                     .WhereNotNull()
                     .DistinctUntilChanged()
@@ -122,6 +83,55 @@ namespace DowUmg.Presentation.ViewModels
                     })
                     .DisposeWith(d);
             });
+        }
+
+        private GenerationSettings CreateGenerationSettings()
+        {
+            var settings = new GenerationSettings()
+            {
+                Mod = ModViewModel.SelectedItem.GetItem<DowMod>(),
+                Maps = GeneralTabViewModel
+                    .Maps.Items.Where(map => map.ToggleItem.IsToggled)
+                    .Select(map => map.Model)
+                    .ToList(),
+                Rules = GeneralTabViewModel
+                    .WinConditions.Items.Where(rule => rule.ToggleItem.IsToggled)
+                    .Select(rule => rule.Model)
+                    .ToList(),
+                Races = TeamTabViewModel
+                    .Races.Items.Where(race => race.ToggleItem.IsToggled)
+                    .Select(race => race.Model)
+                    .ToList(),
+                Players = TeamTabViewModel.Players.Select(player => player.Name.Trim()).ToList(),
+                MinComputer = TeamTabViewModel.MinComputers.SelectedItem.GetItem<int>(),
+                MaxComputer = TeamTabViewModel.MaxComputers.SelectedItem.GetItem<int>(),
+                MinTeams = TeamTabViewModel.MinTeams.SelectedItem.GetItem<int>(),
+                MaxTeams = TeamTabViewModel.MaxTeams.SelectedItem.GetItem<int>(),
+                RandomPositions = TeamTabViewModel.RandomPositions,
+                OneRaceTeams = TeamTabViewModel.OneRaceTeams,
+            };
+
+            foreach (var diff in GameTabViewModel.DiffOption)
+            {
+                settings.GameDifficultyTickets[(int)diff.Model] = diff.NumberInput.Input;
+            }
+
+            foreach (var speed in GameTabViewModel.SpeedOption)
+            {
+                settings.GameSpeedTickets[(int)speed.Model] = speed.NumberInput.Input;
+            }
+
+            foreach (var rate in GameTabViewModel.RateOption)
+            {
+                settings.ResourceRateTickets[(int)rate.Model] = rate.NumberInput.Input;
+            }
+
+            foreach (var start in GameTabViewModel.StartingOption)
+            {
+                settings.StartResourceTickets[(int)start.Model] = start.NumberInput.Input;
+            }
+
+            return settings;
         }
 
         private void OnTeamUpdated(int selectedPlayers, int selectedMinComputer)
@@ -150,6 +160,6 @@ namespace DowUmg.Presentation.ViewModels
 
         public ReactiveCommand<DowMod, Unit> RefreshMod { get; }
 
-        public ReactiveCommand<Unit, IRoutableViewModel> GenerateMatchupAction { get; }
+        public ReactiveCommand<Unit, IRoutableViewModel> GenerateMatchupAction { get; set; }
     }
 }
