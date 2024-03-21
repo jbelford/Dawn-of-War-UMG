9d4fe1fed120e1d46a7eb98673ff2c34fe9a64e3
Jack Belford <jackbelford12@live.ca>
2024-03-20 19:41:39 -0700

    Refactor interface

---
+++ src/DowUmg/Services/IModDataService.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DowUmg.Data.Entities;
+
+namespace DowUmg.Services
+{
+    public interface IModDataService
+    {
+        public List<DowMod> GetMods();
+        public Task<List<DowMod>> GetPlayableMods();
+        public Task<List<DowMap>> GetAddonMaps();
+
+        public void Add(IEnumerable<DowMod> mods);
+
+        public void MigrateData();
+
+        public void DropModData();
+
+        public Task<List<DowMap>> GetModMaps(int modId);
+        public Task<List<GameRule>> GetModRules(int modId);
+        public Task<List<DowRace>> GetRaces(int modId);
+
+        public DowMap GetDefaultMap();
+    }
+}
--- src/DowUmg/Services/ModDataService.cs
+++ src/DowUmg/Services/ModDataService.cs
@@ -7,25 +7,6 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DowUmg.Services
 {
-    public interface IModDataService
-    {
-        public List<DowMod> GetMods();
-        public Task<List<DowMod>> GetPlayableMods();
-        public Task<List<DowMap>> GetAddonMaps();
-
-        public void Add(IEnumerable<DowMod> mods);
-
-        public void MigrateData();
-
-        public void DropModData();
-
-        public Task<List<DowMap>> GetModMaps(int modId);
-        public Task<List<GameRule>> GetModRules(int modId);
-        public Task<List<DowRace>> GetRaces(int modId);
-
-        public DowMap GetDefaultMap();
-    }
-
     internal class ModDataService : IModDataService
     {
         public void Add(IEnumerable<DowMod> mods)
